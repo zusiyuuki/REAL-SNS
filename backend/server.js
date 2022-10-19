7b1c93999772b15c1f10958c14bd0ed491d2d42b
zusiyuuki <zusiyuuki@gmail.com>
2022-10-20 05:29:15 +0900

    ser

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
 //expressの有効化
-const express =require("express")
-const app =express();
-const userRoute =require("./routes/users")
-const authRoute =require("./routes/auth")
-const postRoute =require("./routes/post")
-const mongoose =require("mongoose");
-const uploadRoute =require("./routes/upload")
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+const mongoose = require("mongoose");
+const uploadRoute = require("./routes/upload");
 require("dotenv").config();
-const path =require("path");
+const path = require("path");
 const { dirname } = require("path");
 
 //PORT番号の設定
-const PORT =5000;
+const PORT = process.env.PORT || 5000;
 
 //データベース接続
-mongoose.connect(
-    process.env.MONGOURL
-).then(()=>{
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
     console.log("DBと接続中");
-
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log(err);
-})
+  });
 
 //ミドルウェア
-app.use("/images",express.static(path.join(__dirname,"public/images")))
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
-app.use("/api/users",userRoute);
-app.use("/api/auth",authRoute);
-app.use("/api/post",postRoute);
-app.use("/api/upload",uploadRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
+app.use("/api/upload", uploadRoute);
 
-app.get("/",(req,res)=>{
-    res.send("heloo express");
-})
+app.get("/", (req, res) => {
+  res.send("heloo express");
+});
 
-
-app.listen(PORT,()=> console.log("サーバーが起動しました"));
+app.listen(PORT, () => console.log("サーバーが起動しました"));
